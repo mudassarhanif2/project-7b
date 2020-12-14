0d9db8497eb62be94b04eef15ee2f1a58bbfe2b2
Mudassar Hanif <mudassarhanif2@gmail.com>
2020-12-14 21:47:26 +0500

    changed enzyme-adapter17

---
+++ src/components/App/App.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Timer from '../Timer/Timer';
+
+describe("App component renders", () => {
+
+    let container: any;
+
+    beforeEach(() => (container = shallow(<App />)))
+
+    it('should render a div', () => {
+        expect(container.find('div').length).toEqual(1);
+    })
+
+    it("should render the timer component", () => {
+        expect(container.containsMatchingElement(<Timer />)).toEqual(true);
+    })
+})
